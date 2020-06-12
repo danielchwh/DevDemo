a38f2666e5235233ed5422b806e43277c925b8ba
danielchwh <danielchwh@gmail.com>
2020-06-12 11:02:35 +0800

    add async task roomdatabase

--- app/src/main/java/com/danielchwh/devdemo/RoomDatabase.java
+++ app/src/main/java/com/danielchwh/devdemo/RoomDatabase.java
@@ -1,8 +1,8 @@
 package com.danielchwh.devdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.room.Room;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,6 +10,7 @@ import android.widget.TextView;
 
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.ExecutionException;
 
 public class RoomDatabase extends AppCompatActivity {
     RoomDatabase_Datebase database;
@@ -26,8 +27,7 @@ public class RoomDatabase extends AppCompatActivity {
         buttonDelete = findViewById(R.id.buttonDelete);
         textViewDisplay = findViewById(R.id.textViewDisplay);
 
-        database = Room.databaseBuilder(this, RoomDatabase_Datebase.class, "RoomDatabase_Datebase")
-                .allowMainThreadQueries().build();
+        database = RoomDatabase_Datebase.getDatabase(this);
         dao = database.getDao();
         updateView();
 
@@ -36,7 +36,7 @@ public class RoomDatabase extends AppCompatActivity {
             public void onClick(View v) {
                 int number = new Random().nextInt(100) + 1;
                 RoomDatabase_Entity entity = new RoomDatabase_Entity(number);
-                dao.insert(entity);
+                new InsertAsyncTask(dao).execute(entity);
                 updateView();
             }
         });
@@ -44,19 +44,67 @@ public class RoomDatabase extends AppCompatActivity {
         buttonDelete.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                dao.deleteFirst();
+                new DeleteFirstAsyncTask(dao).execute();
                 updateView();
             }
         });
     }
 
     void updateView() {
-        List<RoomDatabase_Entity> list = dao.getAll();
+        List<RoomDatabase_Entity> list = null;
+        try {
+            list = new GetAllAsyncTask(dao).execute().get();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         String text = "";
         for (int i = 0; i < list.size(); i++) {
             RoomDatabase_Entity entity = list.get((i));
             text += "Round " + entity.getId() + ": " + entity.getNumber() + "\n";
         }
         textViewDisplay.setText(text);
+    }
+
+    static class InsertAsyncTask extends AsyncTask<RoomDatabase_Entity, Void, Void> {
+        private RoomDatabase_Dao dao;
+
+        public InsertAsyncTask(RoomDatabase_Dao dao) {
+            this.dao = dao;
+        }
+
+        @Override
+        protected Void doInBackground(RoomDatabase_Entity... roomDatabase_entities) {
+            dao.insert(roomDatabase_entities);
+            return null;
+        }
+    }
+
+    static class DeleteFirstAsyncTask extends AsyncTask<Void, Void, Void> {
+        private RoomDatabase_Dao dao;
+
+        public DeleteFirstAsyncTask(RoomDatabase_Dao dao) {
+            this.dao = dao;
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            dao.deleteFirst();
+            return null;
+        }
+    }
+
+    static class GetAllAsyncTask extends AsyncTask<Void, Void, List<RoomDatabase_Entity>> {
+        private RoomDatabase_Dao dao;
+
+        public GetAllAsyncTask(RoomDatabase_Dao dao) {
+            this.dao = dao;
+        }
+
+        @Override
+        protected List<RoomDatabase_Entity> doInBackground(Void... voids) {
+            return dao.getAll();
+        }
     }
 }
--- app/src/main/java/com/danielchwh/devdemo/RoomDatabase_Datebase.java
+++ app/src/main/java/com/danielchwh/devdemo/RoomDatabase_Datebase.java
@@ -1,9 +1,20 @@
 package com.danielchwh.devdemo;
 
+import android.content.Context;
+
 import androidx.room.Database;
+import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
 @Database(entities = {RoomDatabase_Entity.class}, version = 1, exportSchema = false)
 public abstract class RoomDatabase_Datebase extends RoomDatabase {
+    private static RoomDatabase_Datebase database;
+    static synchronized RoomDatabase_Datebase getDatabase(Context context) {
+        if (database == null) {
+            database = Room.databaseBuilder(context.getApplicationContext(), RoomDatabase_Datebase.class, "RoomDatabase_Datebase")
+                    .build();
+        }
+        return database;
+    }
     public abstract RoomDatabase_Dao getDao();
 }
