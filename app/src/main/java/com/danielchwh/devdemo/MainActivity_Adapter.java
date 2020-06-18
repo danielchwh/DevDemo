843c1ddd3d57451f89c514bc153b40cb9a325194
danielchwh <55081215+danielchwh@users.noreply.github.com>
2020-06-18 15:25:30 +0800

    Add footer

--- app/src/main/java/com/danielchwh/devdemo/MainActivity_Adapter.java
+++ app/src/main/java/com/danielchwh/devdemo/MainActivity_Adapter.java
@@ -1,5 +1,6 @@
 package com.danielchwh.devdemo;
 
+import android.content.ContentValues;
 import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,8 +12,10 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
 
-public class MainActivity_Adapter extends RecyclerView.Adapter<MainActivity_Adapter.ContentsViewHolder> {
+public class MainActivity_Adapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     private List<MainActivity_Item> allItems;
+    private static final int NORMAL_VIEW_TYPE = 0;
+    private static final int FOOTER_VIEW_TYPE = 1;
 
     public MainActivity_Adapter(List<MainActivity_Item> allItems) {
         this.allItems = allItems;
@@ -20,32 +23,49 @@ public class MainActivity_Adapter extends RecyclerView.Adapter<MainActivity_Adap
 
     @NonNull
     @Override
-    public ContentsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
-        View itemView = layoutInflater.inflate(R.layout.activity_main_item, parent, false);
-        return new ContentsViewHolder(itemView);
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        if (viewType == NORMAL_VIEW_TYPE) {
+            LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+            View itemView = layoutInflater.inflate(R.layout.activity_main_item, parent, false);
+            return new ContentsViewHolder(itemView);
+        } else {
+            LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+            View itemView = layoutInflater.inflate(R.layout.footer_main, parent, false);
+            return new FooterViewHolder(itemView);
+        }
     }
 
     @Override
-    public void onBindViewHolder(@NonNull final ContentsViewHolder holder, final int position) {
-        // Get item from list and display its content in textView
-        MainActivity_Item item = allItems.get(position);
-        holder.itemName.setText(item.getItemName());
-        holder.itemDescription.setText(item.getItemDescription());
-        holder.itemView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Set up button click event
-                Intent intent = new Intent();
-                intent.setClassName(holder.itemView.getContext(), item.getClassFullName());
-                holder.itemView.getContext().startActivity(intent);
-            }
-        });
+    public void onBindViewHolder(@NonNull final RecyclerView.ViewHolder holder, final int position) {
+        if (holder instanceof ContentsViewHolder) {
+            ContentsViewHolder cHolder = (ContentsViewHolder) holder;
+            // Get item from list and display its content in textView
+            MainActivity_Item item = allItems.get(position);
+            cHolder.itemName.setText(item.getItemName());
+            cHolder.itemDescription.setText(item.getItemDescription());
+            cHolder.itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    // Set up button click event
+                    Intent intent = new Intent();
+                    intent.setClassName(cHolder.itemView.getContext(), item.getClassFullName());
+                    cHolder.itemView.getContext().startActivity(intent);
+                }
+            });
+        }
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (position == getItemCount() - 1)
+            return FOOTER_VIEW_TYPE;
+        else
+            return NORMAL_VIEW_TYPE;
     }
 
     @Override
     public int getItemCount() {
-        return allItems.size();
+        return allItems.size() + 1;
     }
 
     static class ContentsViewHolder extends RecyclerView.ViewHolder {
@@ -56,6 +76,12 @@ public class MainActivity_Adapter extends RecyclerView.Adapter<MainActivity_Adap
             // Get view and context
             itemName = itemView.findViewById(R.id.textView1_Main);
             itemDescription = itemView.findViewById(R.id.textView2_Main);
+        }
+    }
+
+    static class FooterViewHolder extends RecyclerView.ViewHolder {
+        public FooterViewHolder(@NonNull View itemView) {
+            super(itemView);
         }
     }
 }
