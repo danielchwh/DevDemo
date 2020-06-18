31cad9064512fe15073d03c2bad0cf60157d9cc5
danielchwh <55081215+danielchwh@users.noreply.github.com>
2020-06-18 20:02:02 +0800

    Add send button to Fcm Demo

--- app/src/main/java/com/danielchwh/devdemo/FcmDemo.java
+++ app/src/main/java/com/danielchwh/devdemo/FcmDemo.java
@@ -2,17 +2,35 @@ package com.danielchwh.devdemo;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.InstanceIdResult;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class FcmDemo extends AppCompatActivity {
+
+    private static final String FCM_API = "https://fcm.googleapis.com/fcm/send";
+    private static final String serverKey = "key=" + "AAAAQY4PQrs:APA91bEfeUbb__igMn9yfGIRkesvgLhl7jrsz_fX_jnY27GpQIyjyEZ3_xb0aJBW2W1MBTsFlM7DOG_q91hqESXaIiPBcsEJZiLBU6qetpjh5MAWNhPrMmwF96cmFLKQVc55QQf0q_cl";
+    private static final String contentType = "application/json";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +41,7 @@ public class FcmDemo extends AppCompatActivity {
         getSupportActionBar().setTitle(R.string.fcm);
 
         EditText editText = findViewById(R.id.editText_Fcm);
+        Button button = findViewById(R.id.button_Fcm);
 
         FirebaseInstanceId.getInstance().getInstanceId()
                 .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
@@ -39,6 +58,51 @@ public class FcmDemo extends AppCompatActivity {
                         Log.d("mylog", "Fcm token: " + token);
                     }
                 });
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                JSONObject notification = new JSONObject();
+                JSONObject notifcationBody = new JSONObject();
+                try {
+                    notifcationBody.put("title", "Test FCM");
+                    notifcationBody.put("body", "Test FCM");
+                    notification.put("to", editText.getText().toString());
+                    notification.put("notification", notifcationBody);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                sendNotification(notification);
+            }
+        });
+    }
+
+    private void sendNotification(JSONObject notification) {
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                FCM_API,
+                notification,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d("mylog", "onResponse: " + response);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("mylog", "onErrorResponse: " + error);
+                    }
+                }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("Authorization", serverKey);
+                params.put("Content-Type", contentType);
+                return params;
+            }
+        };
+        RequestQueue queue = Volley.newRequestQueue(this);
+        queue.add(jsonObjectRequest);
     }
 
 }
